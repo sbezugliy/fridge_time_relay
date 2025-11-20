549f32a9a3319f88b1770f6e8358b4164a4f07b0
Sergey Bezugliy <s.bezugliy@gmail.com>
2025-11-20 13:54:48 +0200

    Refactor

--- fridge_time_relay.ino
+++ fridge_time_relay.ino
@@ -1,146 +1,144 @@
 
 #include <LiquidCrystal_I2C.h>
+#include <stdio.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+const uint8_t LCD_COLUMNS = 16;
+const uint8_t LCD_ROWS = 2;
+const uint8_t RELAY_PIN = 3;
 
-#define Relay 3
+LiquidCrystal_I2C lcd(0x27, LCD_COLUMNS, LCD_ROWS);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-bool initScreen = true;
+struct PhaseConfig {
+  const char *label;
+  int durationSeconds;
+  bool relayConnected;
+};
 
-int timerState[2] = { 0, 0 };
-
-int currentTick = 0;
-
-const int connectedTime[2] = { 30, 00};
-
-const int disconnectedTime[2] = { 45, 00};
-
-int calculateSeconds(int *timeArray){
-  return timeArray[0] * 60 + timeArray[1];
+int minutesToSeconds(int minutes, int seconds) {
+  return minutes * 60 + seconds;
 }
 
-const int connectedTicks = calculateSeconds(connectedTime);
+const PhaseConfig PHASES[] = {
+  {"Freeze", minutesToSeconds(30, 0), true},
+  {"Rest", minutesToSeconds(45, 0), false}
+};
 
-const int disconnectedTicks = calculateSeconds(disconnectedTime);
+const size_t PHASE_COUNT = sizeof(PHASES) / sizeof(PHASES[0]);
 
-bool relayState = true;
+const bool SHOW_INIT_SCREEN = true;
+
+bool relayState = false;
+size_t currentPhaseIndex = 0;
+int elapsedSeconds = 0;
 
 void setup() {
-
   lcd.init();
   lcd.backlight();
-  
-  if (initScreen) printInitScreen();
-  
+
+  if (SHOW_INIT_SCREEN) {
+    printInitScreen();
+  }
+
   initPins();
-  relayConnect();
-  delay(1000);
+  activatePhase(currentPhaseIndex);
 }
 
 void loop() {
-  lcd.clear();
-
-  connectedTimer();
-  disconnectedTimer();
-
+  const PhaseConfig &phase = PHASES[currentPhaseIndex];
+  displayPhase(phase);
   delay(1000);
-}
+  advanceTimer();
 
-void connectedTimer(){
-  if (relayState) {
-    if(checkTimer(connectedTicks)) relayDisconnect();
-    printConnectedTime();
+  if (elapsedSeconds >= phase.durationSeconds) {
+    switchToNextPhase();
   }
-}
-
-void disconnectedTimer(){
-  if (!relayState) {
-    if(checkTimer(disconnectedTicks)) relayConnect();
-    printDisconnectedTime();
-  }
-}
-
-bool checkTimer(int *ticks){
-  increaseTimerState();
-  if (currentTick >= ticks) {
-    resetTimerState();
-    return true;
-  }
-  return false;
-}
-
-void increaseTimerState(){
-  ++currentTick;
-  if(timerState[1] < 59) ++timerState[1];
-  else if (timerState[1] >= 59) {
-    ++timerState[0];
-    timerState[1] = 0;
-  }
-}
-
-void resetTimerState(){
-  timerState[0] = 0;
-  timerState[1] = 0;
-  currentTick = 0;
 }
 
 void initPins() {
-  pinMode(Relay, OUTPUT);
+  pinMode(RELAY_PIN, OUTPUT);
 }
 
-void relayConnect(){
+void switchToNextPhase() {
+  size_t nextIndex = (currentPhaseIndex + 1) % PHASE_COUNT;
+  activatePhase(nextIndex);
+}
+
+void activatePhase(size_t phaseIndex) {
+  currentPhaseIndex = phaseIndex;
+  resetTimerState();
+  applyRelayState(PHASES[currentPhaseIndex].relayConnected);
+  lcd.clear();
+}
+
+void applyRelayState(bool shouldConnect) {
+  if (shouldConnect) {
+    relayConnect();
+  } else {
+    relayDisconnect();
+  }
+}
+
+void advanceTimer() {
+  ++elapsedSeconds;
+}
+
+void resetTimerState() {
+  elapsedSeconds = 0;
+}
+
+void relayConnect() {
   relayState = true;
-  digitalWrite(Relay, HIGH);
-};
+  digitalWrite(RELAY_PIN, HIGH);
+}
 
-void relayDisconnect(){
+void relayDisconnect() {
   relayState = false;
-  digitalWrite(Relay, LOW);
-};
-
-void printConnectedTime(){
-  lcd.setCursor(0, 0);
-  lcd.print("Freeze");
-  lcd.setCursor(0, 1);
-  printTimer(connectedTime, connectedTicks);
+  digitalWrite(RELAY_PIN, LOW);
 }
 
-void printDisconnectedTime(){
-  lcd.setCursor(0, 0);
-  lcd.print("Rest");
-  lcd.setCursor(0, 1);
-  printTimer(disconnectedTime, disconnectedTicks);
+void displayPhase(const PhaseConfig &phase) {
+  printLine(0, phase.label);
+
+  char timerLine[17];
+  buildTimerLine(timerLine, sizeof(timerLine), elapsedSeconds, phase.durationSeconds);
+  printLine(1, timerLine);
 }
 
-void printTimer(int *timeArray, int ticks) {
-  lcd.print(timerState[0]);
-  lcd.print(":");
-  lcd.print(timerState[1]);
-  lcd.print("/");
-  lcd.print(timeArray[0]);
-  lcd.print(":");
-  lcd.print(timeArray[1]);
+void buildTimerLine(char *buffer, size_t length, int elapsed, int target) {
+  char elapsedBuffer[8];
+  char targetBuffer[8];
+  formatTime(elapsedBuffer, sizeof(elapsedBuffer), elapsed);
+  formatTime(targetBuffer, sizeof(targetBuffer), target);
+  snprintf(buffer, length, "%s/%s", elapsedBuffer, targetBuffer);
+}
+
+void formatTime(char *buffer, size_t length, int totalSeconds) {
+  int minutes = totalSeconds / 60;
+  int seconds = totalSeconds % 60;
+  snprintf(buffer, length, "%02d:%02d", minutes, seconds);
+}
+
+void printLine(uint8_t row, const char *text) {
+  lcd.setCursor(0, row);
+  uint8_t col = 0;
+  while (col < LCD_COLUMNS && text[col] != '\0') {
+    lcd.print(text[col]);
+    ++col;
+  }
+  while (col < LCD_COLUMNS) {
+    lcd.print(' ');
+    ++col;
+  }
 }
 
 void printInitScreen() {
-  lcd.setCursor(0, 0);
-  lcd.print("     Fridge     ");
-  lcd.setCursor(0, 1);
-  lcd.print("   Time Relay   ");
+  printLine(0, "     Fridge     ");
+  printLine(1, "   Time Relay   ");
   delay(1000);
-  lcd.clear(); 
+  lcd.clear();
 
-  lcd.setCursor(0, 0);
-  lcd.print("Sergey Bezugliy");
-  lcd.setCursor(3, 1);
-  lcd.print("codenv.top");
+  printLine(0, "Sergey Bezugliy");
+  printLine(1, "   codenv.top   ");
   delay(1000);
-  lcd.clear(); 
-};
-
-void printParameter(char *parameterAbbreviation, int startCol, int Row, int *valueRef) {
-  lcd.setCursor(startCol, Row);
-  lcd.print(parameterAbbreviation);
-  lcd.setCursor(strlen(parameterAbbreviation) + startCol, Row);
-  lcd.print(*valueRef);
+  lcd.clear();
 }
